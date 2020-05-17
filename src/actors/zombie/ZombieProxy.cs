1924f7c49c850b9f810844c7cffa16fd5207adca
iivchenko <iivchenko@live.com>
2020-05-17 14:42:26 +0200

    Add Zombie simple follow player behavior

--- src/actors/zombie/ZombieProxy.cs
+++ src/actors/zombie/ZombieProxy.cs
@@ -1,23 +1,7 @@
-using Godot;
-using System;
-
-public class ZombieProxy : KinematicBody2D
+namespace GodotZombieShooter
 {
-	[Export]
-	public int Age {get;set;}
-	// Declare member variables here. Examples:
-	// private int a = 2;
-	// private string b = "text";
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+	public sealed class ZombieProxy : Zombie
 	{
-		
 	}
-
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
 }
--- src/actors/zombie/zombie.fs
+++ src/actors/zombie/zombie.fs
@@ -4,3 +4,24 @@ open Godot
 
 type public Zombie () =
     inherit KinematicBody2D ()
+
+    let mutable target: Player option = None
+    let mutable speed = 100.0f
+
+    [<Export>]
+    member _.Speed with get() = speed and set(value) = speed <- value
+
+    override _._PhysicsProcess(_: float32) = 
+    
+        match target with 
+        | Some t -> 
+            base.LookAt(t.Position)
+            let direction = (t.Position - base.Position).Normalized()
+            let velocity = direction * speed
+            base.MoveAndSlide(velocity) |> ignore
+        | None -> ()
+        
+    
+    member _.OnPlayerDetected (player: Player) = target <- Some player
+
+    member _.OnPlayerLost (_: Player) = target <- None
