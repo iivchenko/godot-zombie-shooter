a5c2fdd696d30c8b00cee6f040ecafeb0085e1ab
iivchenko <iivchenko@live.com>
2020-05-21 22:25:10 +0200

    Add foot step and gun shot sfx to player

--- src/actors/Player/Player.fs
+++ src/actors/Player/Player.fs
@@ -5,13 +5,17 @@ open Godot
 type public Player () as this =
     inherit KinematicBody2D ()
 
-    [<Export>] 
-    let mutable hitFactory : PackedScene = null
-
     let target = lazy(this.GetNode<Sprite>(NodePath("TargetRay/Target")))
     let targetRay = lazy(this.GetNode<RayCast2D>(NodePath("TargetRay")))
+    let step = lazy(this.GetNode<AudioStreamPlayer2D>(NodePath("Audio/Step")))
+    let gunShot = lazy(this.GetNode<AudioStreamPlayer2D>(NodePath("Audio/GunShot")))
 
+    let mutable audio = false
     let mutable maxSpeed = 100.0f
+    let mutable velocity = Vector2.Zero
+
+    [<Export>] 
+    let mutable hitFactory : PackedScene = null
 
     [<Export>]
     member _.MaxSpeed
@@ -22,6 +26,12 @@ type public Player () as this =
 
     override _._Process(_: float32) = 
 
+        match velocity <> Vector2.Zero with
+        | true when not audio ->
+            audio <- true
+            step.Value.Play()
+        | _ -> ()
+
         target.Value.GlobalPosition <- this.GetGlobalMousePosition()
         targetRay.Value.CastTo <- target.Value.Position
 
@@ -31,11 +41,14 @@ type public Player () as this =
             hit.GlobalPosition <- if targetRay.Value.IsColliding() then targetRay.Value.GetCollisionPoint() else target.Value.GlobalPosition
             base.GetTree().CurrentScene.AddChild(hit)
             hit.Hit()
+            gunShot.Value.Play()
         | _ -> ()
 
     override this._PhysicsProcess (_: float32) =
         
         let direction = Vector2(Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left"), Input.GetActionStrength("move_down") - Input.GetActionStrength("move_up")) 
-        let velocity = direction * maxSpeed
+        velocity <- direction * maxSpeed
         this.LookAt(this.GetGlobalMousePosition())
-        this.MoveAndSlide(velocity) |> ignore
+        velocity <- this.MoveAndSlide(velocity)
+
+    member _.OnStepAudioFinished() = audio <- false
