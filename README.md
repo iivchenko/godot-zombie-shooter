a72b58d2eda47fbf6f8f4bfc6a48eb933b9e6d8e
iivchenko <iivchenko@live.com>
2020-05-23 16:38:14 +0200

    Implement main scene; Introduce kenney ui theme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # godot-zombie-shooter
 Yet another game dev learn project based on godot engine
+
+# Assets
+ - Grass Step https://freesound.org/people/DRFX/sounds/347371/
+ - Sniper Shot https://freesound.org/people/LeMudCrab/sounds/163460/
+ - ADD kenney assets
---
+++ src/MainMenu/MainMenu/MainMenu.fs
@@ -0,0 +1,22 @@
+﻿namespace GodotZombieShooter
+
+open Godot
+
+type public MainMenu () as this =
+    inherit Control ()
+
+    let buttonHover = lazy(this.GetNode<AudioStreamPlayer>(new NodePath("Sounds/ButtonHover")))
+    let buttonClick = lazy(this.GetNode<AudioStreamPlayer>(new NodePath("Sounds/ButtonClick")))
+
+    [<Export>]
+    let mutable NextScene = ""
+
+    member this.OnStartPressed() = 
+        buttonClick.Value.Play()
+        this.GetTree().ChangeScene(NextScene)
+
+    member this.OnExitPressed() = 
+        buttonClick.Value.Play()
+        this.GetTree().Quit()
+
+    member _.OnButtonHover() = buttonHover.Value.Play()
